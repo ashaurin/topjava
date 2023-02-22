11fd1254245fc68e45287be851602340070ac329
ashaurin <55972419+ashaurin@users.noreply.github.com>
2023-02-22 16:11:45 +0300

    2_6_dependency_injection

--- src/main/java/ru/javawebinar/topjava/SpringMain.java
+++ src/main/java/ru/javawebinar/topjava/SpringMain.java
@@ -1,8 +1,13 @@
 package ru.javawebinar.topjava;
 
+import org.springframework.beans.BeansException;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
+import ru.javawebinar.topjava.service.UserService;
+import ru.javawebinar.topjava.web.user.AdminRestController;
 
 import java.util.Arrays;
 
@@ -14,6 +19,19 @@ public class SpringMain {
 //        UserRepository userRepository = (UserRepository) appCtx.getBean("inmemoryUserRepository");
         UserRepository userRepository = appCtx.getBean(UserRepository.class);
         userRepository.getAll();
+
+        UserService userService = appCtx.getBean(UserService.class);
+        userService.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+
         appCtx.close();
+        // java 7 automatic resource management (ARM)
+        try { appCtx = new ClassPathXmlApplicationContext("spring/spring-app.xml"); {
+            System.out.println("Bean definition names: " + Arrays.toString(appCtx.getBeanDefinitionNames()));
+            AdminRestController adminUserController = appCtx.getBean(AdminRestController.class);
+            adminUserController.create(new User(null, "userName", "email@mail.ru", "password", Role.ADMIN));
+        }
+    } catch (BeansException e) {
+            e.printStackTrace();
+        }
     }
 }
